d8b7f21ba8f673dd066e8cfc06126082aef2ad31
Jonathan <j_passe@live.fr>
2016-07-24 14:38:05 +0100

    use module export on a function and a variable

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var http = require('http');
+var module1 = require('./module1');
+var module2 = require('./module2');
 
 function onRequest(request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('Hello world');
+  response.write(module2.myVariable);
+  module1.myFunction();
   response.end();
 }
 
